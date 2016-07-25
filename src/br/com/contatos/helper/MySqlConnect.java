615e2fa6aabf53a888e806edf7c0face18edb1a9
KASSIANO RESENDE <sn1041520@cfp127aluno.local>
2016-07-25 15:36:53 -0300

    primeiro commit

---
+++ src/br/com/contatos/helper/MySqlConnect.java
@@ -0,0 +1,27 @@
+package br.com.contatos.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySqlConnect {
+
+
+	public static Connection ConectarDb(){
+
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con = DriverManager
+					.getConnection("jdbc:mysql://10.107.134.52/contatos", "root", "root");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return con;
+
+	}
+}
